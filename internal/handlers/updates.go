e862fe1be0c4a65b7341cb56cef6732a04bb0064
Lev <levrevera@yandex.ru>
2025-08-05 13:52:15 +0300

    project without ticker implementation

--- internal/handlers/updates.go
+++ internal/handlers/updates.go
@@ -9,14 +9,24 @@ import (
 	"go.uber.org/zap"
 )
 
+type UserState int
+
+const (
+	StateDefault UserState = iota
+	StateWaitingForCity
+	StateWaitingForInterval
+)
+
+var userStates = make(map[int64]UserState)
+
 type Handler struct {
-	bot *tgbot.BotAPI
+	bot      *tgbot.BotAPI
 	owClient *openweather.OpenWeatherClient
 }
 
 func New(bot *tgbot.BotAPI, owClient *openweather.OpenWeatherClient) *Handler {
 	return &Handler{
-		bot: bot,
+		bot:      bot,
 		owClient: owClient,
 	}
 }
@@ -33,48 +43,145 @@ func (h *Handler) Start(log *zap.SugaredLogger) {
 }
 
 func (h *Handler) HandleUpdate(update tgbot.Update, log *zap.SugaredLogger) {
+	chatID := update.Message.Chat.ID
+
 	if update.Message != nil { // if we got a message
-			log.Infof("[%s]: %s", update.Message.From.UserName, update.Message.Text)
+		log.Infof("[%s]: %s", update.Message.From.UserName, update.Message.Text)
 
-			coordinates, err := h.owClient.GetCoordinates(update.Message.Text)
-			if err != nil {
-				log.Error("failed to get coordinates", zap.Error(err))
-				h.bot.Send(tgbot.NewMessage(update.Message.Chat.ID, "Не смог получить координаты"))
+		switch userStates[chatID] {
+		case StateWaitingForCity:
+			h.sendWeatherByCity(update, log)
 
-				return
-			}
-			weather, err := h.owClient.GetWeather(coordinates.Name, coordinates.Lat, coordinates.Lon)
-			if err != nil {
-				log.Error("failed to get weather", zap.Error(err))
-				h.bot.Send(tgbot.NewMessage(update.Message.Chat.ID, "Не смог получить погоду"))
+			userStates[chatID] = StateDefault
+			h.sendMainMenu(chatID)
 
-				return
-			}
+			return
+		case StateWaitingForInterval:
+			// TODO: код тикера
+			userStates[chatID] = StateDefault
+			h.sendMainMenu(chatID)
 
-			msg := tgbot.NewMessage(
-				update.Message.Chat.ID, 
-				fmt.Sprintf("%s\nПогода: %s\nПодробнее: %s\nТемпература: %.1f°C\nОщущается как: %.1f°C\nСкорость ветра: %.1f м/с", coordinates.Name, weather.Weather, weather.Description, weather.Temp, weather.FeelsLike, weather.Speed))
-			msg.ReplyToMessageID = update.Message.MessageID
-
-			h.bot.Send(msg)
+			return
 		}
+
+		switch update.Message.Text {
+		case "/start":
+			h.sendMainMenu(chatID)
+		case "Хочу посмотреть погоду":
+			userStates[chatID] = StateWaitingForCity
+			msg := tgbot.NewMessage(chatID, "Введите название города:")
+			msg.ReplyMarkup = tgbot.ReplyKeyboardRemove{RemoveKeyboard: true}
+			h.bot.Send(msg)
+		case "Хочу запланировать отправку погоды":
+			userStates[chatID] = StateWaitingForInterval
+			h.bot.Send(tgbot.NewMessage(chatID, "Введите интервал, через который вы будете получать оповещение о погоде:\n[не доделано]"))
+			h.sendTimeMenu(chatID)
+		case "Отмена":
+			userStates[chatID] = StateDefault
+			h.sendMainMenu(chatID)
+		}
+
+	}
 }
 
-/* func (h *Handler) ButtonHandler(update tgbot.Update, log *zap.SugaredLogger) {
+func (h *Handler) sendMainMenu(chatID int64) {
 	replyKeyboard := tgbot.NewReplyKeyboard(
 		tgbot.NewKeyboardButtonRow(
 			tgbot.NewKeyboardButton("Хочу посмотреть погоду"),
 			tgbot.NewKeyboardButton("Хочу запланировать отправку погоды"),
 		),
+		tgbot.NewKeyboardButtonRow(tgbot.NewKeyboardButton("Отмена")),
 	)
 
-	switch update.Message.Text {
-	case "Хочу посмотреть погоду":
-		h.bot.Send(tgbot.NewMessage(update.Message.Chat.ID, "Жду город"))
-	case "Хочу запланировать отправку погоды":
-	}
-} 
+	msg := tgbot.NewMessage(chatID, "Главное меню:")
+	msg.ReplyMarkup = replyKeyboard
+	h.bot.Send(msg)
+}
 
-func (h *Handler) WeatherMsg(update tgbot.Update, log *zap.SugaredLogger) {
+func (h *Handler) sendTimeMenu(chatID int64) {
+	replyKeyboard := tgbot.NewReplyKeyboard(
+		tgbot.NewKeyboardButtonRow(
+			tgbot.NewKeyboardButton("10 секунд"),
+			tgbot.NewKeyboardButton("1 минута"),
+		), tgbot.NewKeyboardButtonRow(
+			tgbot.NewKeyboardButton("1 час"),
+			tgbot.NewKeyboardButton("24 часа"),
+		),
+		tgbot.NewKeyboardButtonRow(
+			tgbot.NewKeyboardButton("Ввести свое время в минутах"),
+			tgbot.NewKeyboardButton("Ввести свое время в часах"),
+		),
+	)
+	msg := tgbot.NewMessage(chatID, "Установите интервал:")
+	msg.ReplyMarkup = replyKeyboard
+	h.bot.Send(msg)
+}
+
+func (h *Handler) sendWeatherByCity(update tgbot.Update, log *zap.SugaredLogger) {
 	
+	
+	coordinates, err := h.owClient.GetCoordinates(update.Message.Text)
+	if err != nil {
+		log.Error("failed to get coordinates", zap.Error(err))
+		h.bot.Send(tgbot.NewMessage(update.Message.Chat.ID, "Не смог получить координаты"))
+
+		return
+	}
+	weather, err := h.owClient.GetWeather(coordinates.Name, coordinates.Lat, coordinates.Lon)
+	if err != nil {
+		log.Error("failed to get weather", zap.Error(err))
+		h.bot.Send(tgbot.NewMessage(update.Message.Chat.ID, "Не смог получить погоду"))
+
+		return
+	}
+
+	msg := tgbot.NewMessage(
+		update.Message.Chat.ID,
+		fmt.Sprintf("%s\nПогода: %s\nПодробнее: %s\nТемпература: %.1f°C\nОщущается как: %.1f°C\nСкорость ветра: %.1f м/с", coordinates.Name, weather.Weather, weather.Description, weather.Temp, weather.FeelsLike, weather.Speed))
+
+	msg.ReplyToMessageID = update.Message.MessageID
+
+	h.bot.Send(msg)
+}
+
+/* func (h *Handler) startWeatherTicker(city string, log *zap.SugaredLogger, interval int) {
+	ticker := time.NewTicker(time.Duration(interval) * time.Second)
+	defer ticker.Stop()
+	
+	done := make(chan bool)
+
+	go func() {
+		time.Sleep(time.Second)
+		close(done)
+	}()
+
+	for {
+		select {
+		case _, ok := <-ticker.C:
+			if !ok {
+				return
+			}
+
+		case <-done:
+			return
+		}
+	}
+} */
+
+/* func (h *Handler) getWeather(update tgbot.Update) tgbot.MessageConfig {
+	coordinates, err := h.owClient.GetCoordinates(update.Message.Text)
+	if err != nil {
+		log.Error("failed to get coordinates", zap.Error(err))
+		msg := tgbot.NewMessage(update.Message.Chat.ID, "Не смог получить координаты"))
+
+		return msg
+	}
+	weather, err := h.owClient.GetWeather(coordinates.Name, coordinates.Lat, coordinates.Lon)
+	if err != nil {
+		log.Error("failed to get weather", zap.Error(err))
+		msg := tgbot.NewMessage(update.Message.Chat.ID, "Не смог получить погоду"))
+
+		return msg
+	}
+	return 
 } */
